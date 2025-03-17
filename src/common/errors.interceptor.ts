4d2cb919c041dc1cd2651790811da32f539c05fc
Nho Do <dothinhodut@gmail.com>
2025-03-17 15:29:50 +0700

    chore: set up interceptor

---
+++ src/common/errors.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((err) => {
+        if (err instanceof HttpException) {
+          return throwError(() => err);
+        }
+
+        console.log(err);
+        return throwError(
+          () => new InternalServerErrorException('Internal Server Error'),
+        );
+      }),
+    );
+  }
+}
